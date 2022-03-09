05cd5090bd6f18283b4948550fd9fe8f20676d37
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-03-09 22:41:37 +0200

    working with var, dynamic and anonimous objects

---
+++ VarAndDynamic/VarAndDynamic/CollectionOfIntegers.cs
@@ -0,0 +1,43 @@
+﻿namespace VarAndDynamic
+{
+    public class CollectionOfIntegers
+    {
+        private readonly int[] array;
+
+        public CollectionOfIntegers(params int[] elements)
+        {
+            array = elements ?? new int[0];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < array.Length)
+                {
+                    return array[index];
+                }
+
+                return int.MinValue;
+            }
+            set
+            {
+                if (index >= 0 && index < array.Length)
+                {
+                    array[index] = value;
+                }
+            }
+        }
+
+        public int Sum()
+        {
+            int sum = 0;
+            foreach (int elem in array)
+            {
+                sum += elem;
+            }
+
+            return sum;
+        }
+    }
+}
--- VarAndDynamic/VarAndDynamic/Program.cs
+++ VarAndDynamic/VarAndDynamic/Program.cs
@@ -6,7 +6,53 @@ namespace VarAndDynamic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            long b = 20L;
+            int a = (int)b;
+
+
+            var p1 = new
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            var p2 = new
+            {
+                FirstName = "John2",
+                LastName = "Doe2"
+            };
+
+            /*
+            dynamic a = new CollectionOfIntegers(1, 2, 3);
+            ProcessData(a);
+            PrintInteger(a);
+            */
+
+            /*
+            var a = new CollectionOfIntegers(1, 2, 3);
+            int sum = a.Sum();
+            Console.WriteLine(sum);
+            */
+        }
+
+        private static void ProcessData(dynamic data)
+        {
+            // NOT WORKING!
+            // a = a + 10;
+            // int sum = data.Sum();
+            // Console.WriteLine(sum);
+            Console.WriteLine(data);
+
+            //data = data.ToString() + " test";
+            //Console.WriteLine(data);
+
+            data = data + 20;
+            Console.WriteLine(data);
+        }
+
+        private static void PrintInteger(int x)
+        {
+            Console.WriteLine(x);
         }
     }
 }
